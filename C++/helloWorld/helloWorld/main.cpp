f8b50da78ac36f55ab4fee6183fc0cbff35fba7e
Moroni-MB <moronimontoya04@gmail.com>
2026-03-02 10:45:16 -0700

    c++ projects

---
+++ C++/helloWorld/helloWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  helloWorld
+//
+//  Created by Moroni Montoya Bernal on 2/28/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
---
+++ C++/iBudget/iBudget/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  iBudget
+//
+//  Created by Moroni Montoya Bernal on 3/1/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
